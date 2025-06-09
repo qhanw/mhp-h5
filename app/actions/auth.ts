2f0335fa04942750e8108ef0591d8c9e34115fb6
qhanw <whenhan@foxmail.com>
2025-06-09 21:46:50 +0800

    feat: 注册并登录

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,8 +1,10 @@
 "use server";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import { redirect } from "next/navigation";
 import { SignupFormSchema, FormState } from "@/app/lib/definitions";
 import { createSession, deleteSession } from "@/app/lib/session";
+
+import { db, users } from "@/db";
 
 export async function signup(state: FormState, formData: FormData) {
   // 1. Validate form fields
@@ -17,27 +19,21 @@ export async function signup(state: FormState, formData: FormData) {
     return { errors: validateFields.error.flatten().fieldErrors };
   }
   // 2. Prepare data for insertion into database
-  // const { name, email, password } = validateFields.data;
-  // // e.g. Hash the user's password before storing it
-  // const hashedPassword = await bcrypt.hash(password, 10);
+  const { name, email, password } = validateFields.data;
+  // e.g. Hash the user's password before storing it
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  // // 3. Insert the user into the database or call an Auth Library's API
-  // const data = await db
-  //   .insert(users)
-  //   .values({
-  //     name,
-  //     email,
-  //     password: hashedPassword,
-  //   })
-  //   .returning({ id: users.id });
+  // 3. Insert the user into the database or call an Auth Library's API
+  const data = await db
+    .insert(users)
+    .values({ username: name, email, password: hashedPassword })
+    .returning({ id: users.id });
 
-  // const user = data[0];
+  const user = data[0];
 
-  // if (!user) {
-  //   return {
-  //     message: "An error occurred while creating your account.",
-  //   };
-  // }
+  if (!user) {
+    return { message: "An error occurred while creating your account." };
+  }
 
   // TODO:
   // 4. Create user session
--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { eq } from "drizzle-orm";
-import { users as usersTable } from "./schema";
+// import { eq } from "drizzle-orm";
+// import { users as usersTable } from "./schema";
 
-const db = drizzle(process.env.DATABASE_URL!);
+export * from "./schema";
+
+export const db = drizzle(process.env.DATABASE_URL!);
+
 
 // node-postgres with config
 
@@ -29,32 +32,35 @@ const db = drizzle(process.env.DATABASE_URL!);
 // });
 // const db = drizzle({ client: pool });
 
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    username: "John",
-    password: '30',
-    email: "john@example.com",
-  };
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
-  const users = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users);
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-  await db
-    .update(usersTable)
-    .set({
-      password: '31',
-    })
-    .where(eq(usersTable.email, user.email!));
-  console.log("User info updated!");
-//   await db.delete(usersTable).where(eq(usersTable.email, user.email!));
-//   console.log("User deleted!");
-}
-main();
+
+// Test code
+
+// async function main() {
+//   const user: typeof usersTable.$inferInsert = {
+//     username: "John",
+//     password: "30",
+//     email: "john@example.com",
+//   };
+//   await db.insert(usersTable).values(user);
+//   console.log("New user created!");
+//   const users = await db.select().from(usersTable);
+//   console.log("Getting all users from the database: ", users);
+//   /*
+//   const users: {
+//     id: number;
+//     name: string;
+//     age: number;
+//     email: string;
+//   }[]
+//   */
+//   await db
+//     .update(usersTable)
+//     .set({
+//       password: "31",
+//     })
+//     .where(eq(usersTable.email, user.email!));
+//   console.log("User info updated!");
+//   //   await db.delete(usersTable).where(eq(usersTable.email, user.email!));
+//   //   console.log("User deleted!");
+// }
+// main();

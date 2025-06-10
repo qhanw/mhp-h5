dacf06cad862c6f404e97d9b8092ab8737d6801e
qhanw <whenhan@foxmail.com>
2025-06-10 09:40:58 +0800

    feat: 调整数据库ID为uuid

--- db/schema.ts
+++ db/schema.ts
@@ -4,12 +4,10 @@ import { number } from "zod";
 
 export const users = pgTable("users", {
 	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	// id: uuid().defaultRandom().primaryKey().notNull(),
-    id: bigserial({mode:'number'}).primaryKey(),
+	id: uuid().defaultRandom().primaryKey().notNull(),
 	username: text().notNull(),
 	email: text().unique().notNull(),
 	password: text().notNull(),
 	age: integer(),
 	createdAt: timestamp("created_at", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
 });
-
--- drizzle/0000_pale_iceman.sql
+++ drizzle/0000_pale_iceman.sql
@@ -2,12 +2,12 @@
 -- If you want to run this migration please uncomment this code before executing migrations
 /*
 CREATE TABLE "users" (
-	"id" bigserial PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"username" text NOT NULL,
 	"email" text NOT NULL,
 	"password" text NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
 	"age" integer,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 
